e92db7704f895fe5f5d82438eb695a8264806e45
Gary <garylin0969@gmail.com>
2023-04-03 19:44:16 +0800

    feat(Home): add google_logo image and import the HomeSearch component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import HomeHeader from '@/components/HomeHeader';
+import HomeSearch from '@/components/HomeSearch';
+import Image from 'next/image';
 
 const Home = () => {
     return (
         <>
             <HomeHeader />
+            <div className="flex flex-col items-center mt-24">
+                <Image src="/google_logo.png" width={300} height={100} alt="google logo" />
+                <HomeSearch />
+            </div>
         </>
     );
 };
