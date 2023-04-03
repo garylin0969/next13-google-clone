790b552685a4e977b03f29b31fb06cf4e4974cdc
Gary <garylin0969@gmail.com>
2023-04-04 00:10:58 +0800

    refactor(home header): import SignInButton

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { TbGridDots } from 'react-icons/tb';
+import SignInButton from './SignInButton';
 
 const HomeHeader = () => {
     return (
@@ -13,9 +14,7 @@ const HomeHeader = () => {
                         Images
                     </Link>
                     <TbGridDots className="bg-transparent hover:bg-gray-200 rounded-full text-4xl p-2" />
-                    <button className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-shadow">
-                        Sign in
-                    </button>
+                    <SignInButton />
                 </div>
             </header>
         </>
