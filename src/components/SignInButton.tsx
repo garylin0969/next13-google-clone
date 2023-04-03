27f7fb45d6661d0b25db8ba1c9333129b77ade71
Gary <garylin0969@gmail.com>
2023-04-04 00:10:00 +0800

    feat: create sign in button component

---
+++ src/components/SignInButton.tsx
@@ -0,0 +1,11 @@
+const SignInButton = () => {
+    return (
+        <>
+            <button className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-all">
+                Sign in
+            </button>
+        </>
+    );
+};
+
+export default SignInButton;
