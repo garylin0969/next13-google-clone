caafcabec235ab9b4e02f9d0bc7198ec6cbd88ec
Gary <garylin0969@gmail.com>
2023-04-03 21:03:11 +0800

    feat(CountryLookup): create a countryLookup client component for the footer server component

---
+++ src/components/CountryLookup.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { GeoLocation } from '@/types';
+import axios from 'axios';
+import useSWR from 'swr';
+
+const CountryLookup = () => {
+    const fetcher: (url: string) => Promise<GeoLocation> = (url: string) => axios.get(url).then((res) => res.data);
+    const { data, error } = useSWR('http://ip-api.com/json', fetcher);
+
+    return <>{data ? <div>{data.country}</div> : <div>loading...</div>}</>;
+};
+
+export default CountryLookup;
