7609e2c029bc2fb361fc47a9ace2e1e4840a0fd8
Gary <garylin0969@gmail.com>
2023-04-04 00:09:00 +0800

    feat(search header): make the style and component

--- src/components/SearchHeader.tsx
+++ src/components/SearchHeader.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+import Image from 'next/image';
+import Link from 'next/link';
+import { RiSettings3Line } from 'react-icons/ri';
+import { TbGridDots } from 'react-icons/tb';
+import SearchBox from './SearchBox';
+import SearchHeaderOptions from './SearchHeaderOptions';
+import SignInButton from './SignInButton';
 
 const SearchHeader = () => {
-  return <div>SearchHeader</div>;
+    return (
+        <>
+            <header className="sticky top-0 bg-white">
+                <div className="flex w-full p-6 items-center justify-between">
+                    <Link href="/">
+                        <Image
+                            src="/google_logo.png"
+                            width={120}
+                            height={40}
+                            alt="google logo"
+                            priority
+                            style={{
+                                width: 'auto',
+                                height: 'auto',
+                            }}
+                        />
+                    </Link>
+                    <div className="flex-1">
+                        <SearchBox />
+                    </div>
+                    <div className="hidden md:inline-flex md:mr-2 space-x-2">
+                        <RiSettings3Line className="header-icon" />
+                        <TbGridDots className="header-icon" />
+                    </div>
+                    <SignInButton />
+                </div>
+                <SearchHeaderOptions />
+            </header>
+        </>
+    );
 };
 
 export default SearchHeader;
