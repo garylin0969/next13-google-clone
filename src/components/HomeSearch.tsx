0b668a2491e9d891142632d166eeb56997abdb5c
Gary <garylin0969@gmail.com>
2023-04-03 21:11:56 +0800

    style(home search): add items-center class to align the icon and input

--- src/components/HomeSearch.tsx
+++ src/components/HomeSearch.tsx
@@ -32,7 +32,7 @@ const HomeSearch = () => {
     return (
         <>
             <form
-                className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl"
+                className="flex items-center w-full mt-5 mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl"
                 onSubmit={handleSubmit}
             >
                 <AiOutlineSearch className="text-xl text-gray-500 mr-3" />
