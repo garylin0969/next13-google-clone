c423df1264e9bfde83125319516d43044d22673f
Gary <garylin0969@gmail.com>
2023-04-05 16:32:03 +0800

    refactor: fetcher type

--- src/components/CountryLookup.tsx
+++ src/components/CountryLookup.tsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 import useSWR from 'swr';
 
 const CountryLookup = () => {
-    const fetcher: (url: string) => Promise<GeoLocation> = (url: string) => axios.get(url).then((res) => res.data);
+    const fetcher = (url: string): Promise<GeoLocation> => axios.get(url).then((res) => res.data);
     const { data, error } = useSWR('http://ip-api.com/json', fetcher);
 
     return <>{data ? <div>{data.country}</div> : <div>loading...</div>}</>;
