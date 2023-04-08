1601394480d02b8f4e58bebafaf4a606a3a95e7c
Gary <garylin0969@gmail.com>
2023-04-09 00:00:00 +0800

    fix: correct spelling mistake

--- src/app/search/web/page.tsx
+++ src/app/search/web/page.tsx
@@ -16,7 +16,7 @@ const WebSearchPage = async ({ searchParams }: WebSearchPageProps) => {
             <>
                 <div className="flex flex-col justify-center items-center pt-10">
                     <h1 className="text-3xl mb-4">No results found</h1>
-                    <p className="text-lg">Try searching for someing else or go back to the homepage</p>
+                    <p className="text-lg">Try searching for something else or go back to the homepage</p>
                     <Link className="text-blue-500" href="/">
                         Home
                     </Link>
