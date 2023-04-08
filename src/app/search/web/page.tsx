b4c55960d47ebe5c8346b2abb7bb8f86acf853b7
Gary <garylin0969@gmail.com>
2023-04-08 23:08:48 +0800

    feat: add type definitions

--- src/app/search/web/page.tsx
+++ src/app/search/web/page.tsx
@@ -1,10 +1,16 @@
 import { googleServerApi } from '@/app/api/serverApi';
 import { SearchItem, SearchResults } from '@/types';
 
-const WebSearchPage = async ({ searchParams }: any) => {
+interface WebSearchPage {
+    searchParams: {
+        searchTerm: string;
+    };
+}
+
+const WebSearchPage = async ({ searchParams }: WebSearchPage) => {
     const data: SearchResults = await googleServerApi.searchResults(searchParams.searchTerm);
     const results: SearchItem[] = data.items;
-    return <>{results && results.map((result: any, index: number) => <h1 key={index}>{result.title}</h1>)}</>;
+    return <>{results && results.map((result: SearchItem, index: number) => <h1 key={index}>{result.title}</h1>)}</>;
 };
 
 export default WebSearchPage;
