09e523fc7cb2deaf05ed92cd1c496e6e5ebba276
Gary <garylin0969@gmail.com>
2023-04-08 23:33:29 +0800

    feat: handle possible error

--- src/app/search/web/page.tsx
+++ src/app/search/web/page.tsx
@@ -1,5 +1,6 @@
 import { googleServerApi } from '@/app/api/serverApi';
 import { SearchItem, SearchResults } from '@/types';
+import Link from 'next/link';
 
 interface WebSearchPage {
     searchParams: {
@@ -10,6 +11,19 @@ interface WebSearchPage {
 const WebSearchPage = async ({ searchParams }: WebSearchPage) => {
     const data: SearchResults = await googleServerApi.searchResults(searchParams.searchTerm);
     const results: SearchItem[] = data.items;
+    if (!results) {
+        return (
+            <>
+                <div className="flex flex-col justify-center items-center pt-10">
+                    <h1 className="text-3xl mb-4">No results found</h1>
+                    <p className="text-lg">Try searching for someing else or go back to the homepage</p>
+                    <Link className="text-blue-500" href="/">
+                        Home
+                    </Link>
+                </div>
+            </>
+        );
+    }
     return <>{results && results.map((result: SearchItem, index: number) => <h1 key={index}>{result.title}</h1>)}</>;
 };
 
