38504796b61a380578c2e3133dff8bccf8381cf4
Gary <garylin0969@gmail.com>
2023-04-08 23:02:10 +0800

    feat: fetch data from google search api and shows the title

--- src/app/search/web/page.tsx
+++ src/app/search/web/page.tsx
@@ -1,5 +1,10 @@
-const WebSearchPage = () => {
-    return <div>WebSearchPage</div>;
+import { googleServerApi } from '@/app/api/serverApi';
+import { SearchItem, SearchResults } from '@/types';
+
+const WebSearchPage = async ({ searchParams }: any) => {
+    const data: SearchResults = await googleServerApi.searchResults(searchParams.searchTerm);
+    const results: SearchItem[] = data.items;
+    return <>{results && results.map((result: any, index: number) => <h1 key={index}>{result.title}</h1>)}</>;
 };
 
 export default WebSearchPage;
