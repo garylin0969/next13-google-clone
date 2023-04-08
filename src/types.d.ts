5efb25af8d93d0cd6e63ca45eb474ee7b00a14eb
Gary <garylin0969@gmail.com>
2023-04-08 23:02:55 +0800

    feat: add google search types

--- src/types.d.ts
+++ src/types.d.ts
@@ -14,3 +14,48 @@ export interface GeoLocation {
     timezone: string;
     zip: string;
 }
+
+export interface SearchUrl {
+    type: string;
+    template: string;
+}
+
+export interface Request {
+    kind: string;
+    url: SearchUrl;
+}
+
+export interface Query {
+    request: Request[];
+    nextPage: Request[];
+}
+
+export interface SearchInformation {
+    searchTime: number;
+    formattedSearchTime: string;
+    totalResults: string;
+    formattedTotalResults: string;
+}
+
+export interface SearchItem {
+    kind: string;
+    title: string;
+    htmlTitle: string;
+    link: string;
+    displayLink: string;
+    snippet: string;
+    htmlSnippet: string;
+    cacheId: string;
+    formattedUrl: string;
+    htmlFormattedUrl: string;
+    pagemap: any;
+}
+
+export interface SearchResults {
+    kind: string;
+    url: SearchUrl;
+    queries: Query;
+    context: { title: string };
+    searchInformation: SearchInformation;
+    items: SearchItem[];
+}
