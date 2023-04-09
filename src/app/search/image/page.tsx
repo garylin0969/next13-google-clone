198654ec7999eafc84eebae5220ddbd750631041
Gary <garylin0969@gmail.com>
2023-04-09 18:00:37 +0800

    feat: create image search results

--- src/app/search/image/page.tsx
+++ src/app/search/image/page.tsx
@@ -1,5 +1,19 @@
-const ImageSearchPage = () => {
-    return <div>ImageSearchPage</div>;
+import { googleServerApi } from '@/app/api/serverApi';
+import ImageSearchResults from '@/components/ImageSearchResults';
+import NoResultsFound from '@/components/NoResultsFound';
+import { SearchItem, SearchResults } from '@/types';
+
+interface ImageSearchPageProps {
+    searchParams: {
+        searchTerm: string;
+    };
+}
+
+const ImageSearchPage = async ({ searchParams }: ImageSearchPageProps) => {
+    const data: SearchResults = await googleServerApi.searchResults(searchParams.searchTerm, 'image');
+    const results: SearchItem[] = data.items;
+    if (!results) return <NoResultsFound />;
+    return <>{results && <ImageSearchResults results={data} />}</>;
 };
 
 export default ImageSearchPage;
