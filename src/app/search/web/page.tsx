1b57c3154eaa19d008ba453b4e0e2183f2978ae0
Gary <garylin0969@gmail.com>
2023-04-09 18:01:36 +0800

    refactor: switch to importing specialized component when there is no result

--- src/app/search/web/page.tsx
+++ src/app/search/web/page.tsx
@@ -1,7 +1,7 @@
 import { googleServerApi } from '@/app/api/serverApi';
+import NoResultsFound from '@/components/NoResultsFound';
 import WebSearchResults from '@/components/WebSearchResults';
 import { SearchItem, SearchResults } from '@/types';
-import Link from 'next/link';
 
 interface WebSearchPageProps {
     searchParams: {
@@ -12,19 +12,7 @@ interface WebSearchPageProps {
 const WebSearchPage = async ({ searchParams }: WebSearchPageProps) => {
     const data: SearchResults = await googleServerApi.searchResults(searchParams.searchTerm);
     const results: SearchItem[] = data.items;
-    if (!results) {
-        return (
-            <>
-                <div className="flex flex-col justify-center items-center pt-10">
-                    <h1 className="text-3xl mb-4">No results found</h1>
-                    <p className="text-lg">Try searching for something else or go back to the homepage</p>
-                    <Link className="text-blue-500" href="/">
-                        Home
-                    </Link>
-                </div>
-            </>
-        );
-    }
+    if (!results) return <NoResultsFound />;
     return <>{results && <WebSearchResults results={data} />}</>;
 };
 
