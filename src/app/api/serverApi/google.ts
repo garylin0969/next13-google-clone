3c100ed16fe420883e0cf22d300e9f0417b4cb89
Gary <garylin0969@gmail.com>
2023-04-09 17:56:36 +0800

    feat: add searchType to the search criteria

--- src/app/api/serverApi/google.ts
+++ src/app/api/serverApi/google.ts
@@ -4,13 +4,21 @@ import { staticFetching } from './request';
 const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 const CONTEXT_KEY = process.env.CONTEXT_KEY;
 
-const searchResults = async (searchTerm: string): Promise<SearchResults> => {
-    const params: object = { key: GOOGLE_API_KEY, cx: CONTEXT_KEY, q: searchTerm };
+interface SearchParams {
+    key?: string;
+    cx?: string;
+    q: string;
+    searchType?: string;
+}
+
+const searchResults = async (searchTerm: string, searchType?: string): Promise<SearchResults> => {
+    let params: SearchParams = { key: GOOGLE_API_KEY, cx: CONTEXT_KEY, q: searchTerm };
+    if (searchType) params.searchType = searchType;
     return await staticFetching('https://www.googleapis.com/customsearch/v1', params);
 };
 
 const googleServerApi: {
-    searchResults: (searchTerm: string) => Promise<SearchResults>;
+    searchResults: (searchTerm: string, searchType?: string) => Promise<SearchResults>;
 } = {
     searchResults,
 };
